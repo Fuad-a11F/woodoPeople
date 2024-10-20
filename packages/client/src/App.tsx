a04a226b54ab60f6da2f584a5d268b099afa0996
Alex <alex.bogatov1994@gmail.com>
2024-10-20 15:01:51 -0400

    Вёрстка страницы логина

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 import {
   Forum,
@@ -18,6 +18,8 @@ import { Menu } from './components'
 import './App.css'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
   useEffect(() => {
     const fetchServerData = async () => {
       const url = `http://localhost:${__SERVER_PORT__}`
@@ -29,18 +31,44 @@ function App() {
     fetchServerData()
   }, [])
 
+  const handleLogin = () => {
+    setIsAuthenticated(true)
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Menu />
+        {isAuthenticated && <Menu />}
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/forum" element={<Forum />} />
-          <Route path="/forum-topic" element={<ForumTopic />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/"
+            element={isAuthenticated ? <Main /> : <Navigate to="/login" />}
+          />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route
+            path="/forum"
+            element={isAuthenticated ? <Forum /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/forum-topic"
+            element={
+              isAuthenticated ? <ForumTopic /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/game"
+            element={isAuthenticated ? <Game /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/leaderboard"
+            element={
+              isAuthenticated ? <Leaderboard /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/profile"
+            element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
+          />
           <Route path="/registration" element={<Registration />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
